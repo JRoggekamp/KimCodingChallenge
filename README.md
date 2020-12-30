6ef85b9f2ad3e2ce5c854028c78822b2b2e26c09
Joep Roggekamp <joeproggekamp@mbpvanjoggekamp.home>
2020-12-30 18:32:12 +0100

    added comments

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # KimCodingChallenge
+Als Productowner wil ik een tool hebben waarmee ik de vestigingen die het meeste in mijn buurt zijn kan zijn.
+De applicatie moet een overzicht tonen van maximaal vijf vestigingen bij de gebruiker in de buurt.
+De weergave van de vestiging moet minimaal een naam en een adres bevatten.
+De gebruiker van de applicatie moet in staat zijn om een vestiging aan te klikken en op die wijze naar de website van de vestiging te navigeren.
+
+
+Voor deze opdracht mag je gebruik maken van de cms-services acceptatie omgeving. De documentatie kun je vinden op de volgende link.
+https://acceptatie.monuta.nl/cms-services/swagger-ui.html
+Tevens mag je tijdens deze opdracht geen gebruik maken van frameworks zoals Angular, react, Vue of JQuery.
+Het project moet gebouwd worden d.m.v. van HTML 5, CSS 3 en vanilla JavaScript
+
+
--- index.js
+++ index.js
@@ -8,9 +8,11 @@ searchButton.addEventListener("click", evt => {
   fetch('https://acceptatie.monuta.nl/cms-services/api/plaatsen/'+ locationInput + '/vestigingen')
     .then(res => res.json())
     .then(locations => {
+      //defining the header that will be atop of the search results
     let listTitle = document.createElement('H2');
     listTitle.textContent = "Ik heb de volgende vestiging(en) gevonden:";
     locationDiv.appendChild(listTitle);
+    // iterating through the search results and mapping them to corresponding elements
       for (const location of locations.vestigingen) {
         let locationContainer = document.createElement('div');
         locationContainer.id = "location";
@@ -35,6 +37,7 @@ searchButton.addEventListener("click", evt => {
           urlLink.setAttribute('href', `${location.locatieURL}`),
           urlLink.innerHTML = "Bezoek locatiepagina"
         )
+        //appending all the components to the appropriate div
         locationDiv.appendChild(locationContainer);
         nameContainer.appendChild(listName);
         locationContainer.appendChild(nameContainer);
@@ -48,3 +51,7 @@ searchButton.addEventListener("click", evt => {
     })
     .catch(console.error);
 })
+
+function parseDiv(){
+  return
+}
